cbbea6284a71244fc43934520f4131eb99eacb11
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2025-04-17 16:43:37 +0300

    Added Unit of work

--- Core/DomainLayer/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Core/DomainLayer/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84cf5eb64231fdc0b417037cc4a97a29a4e7e138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+939a675e209481e79b680c6dbfe7d07f46f1a084")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Contracts;
+using Domain.Models;
+using Persistence.Data.DbContexts;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork(StoreDbContext _dbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> _repositories = [];
+
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+           // Get Type Name
+           var typeName = typeof(TEntity).Name;
+
+            // Dic<String, Object> ==> string key [Name of Type] -- Object an object from generic repository
+            //if (_repositories.ContainsKey(typeName)) 
+            //    return (IGenericRepository<TEntity, TKey>) _repositories[typeName];
+            if (_repositories.TryGetValue(typeName, out object? value))
+                return (IGenericRepository<TEntity, TKey>)value;
+
+            else
+            {
+                // Create object
+                var repo = new GenericRepository<TEntity, TKey>(_dbContext);
+                // Store object into dic
+                _repositories["typeName"] = repo;
+                // Return the object
+                return repo;
+            }
+        }
+
+        public async Task<int> SaveChangesAsync() => await _dbContext.SaveChangesAsync();  // save changes
+ 
+    }
+}
